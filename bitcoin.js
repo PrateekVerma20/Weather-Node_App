fa7bd4dbb91f5b95ffb06eefe1a551013ed5bfae
Prateek Verma <prateekverma1998@gmail.com>
2021-02-08 13:23:43 +0530

    added bitcoin.js

--- bitcoin.js
+++ bitcoin.js
@@ -1,8 +1,10 @@
+const requestPromise = require("request-promise");
+
 const fetchBitCoin = async ()=>{
 
     try{
-        //'https://api.cryptonator.com/api/ticker/btc-usd'
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Bilaspur&appid=0c7b5085f73e81688a753c72b9153714`);
+        //
+    const res = await requestPromise('https://api.cryptonator.com/api/ticker/btc-usd');
    // console.log("fetch promise is done!!!!!!");
     const data  = await res.json();
     //console.log("Json is done!!!!!!");
