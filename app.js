4e12f7720dcab85d3c74c6a85d4779974e26213e
Prateek Verma <prateekverma1998@gmail.com>
2021-02-08 12:48:16 +0530

    new feature added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+const { request } = require("express");
+const requestPromise = require("request-promise");
+
 const fetchBitCoin = async ()=>{
 
     try{
-        //'https://api.cryptonator.com/api/ticker/btc-usd'
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Bilaspur&appid=0c7b5085f73e81688a753c72b9153714`);
+        //***`https://api.openweathermap.org/data/2.5/weather?q=Bilaspur&appid=0c7b5085f73e81688a753c72b9153714`'
+    const res = await requestPromise('https://api.cryptonator.com/api/ticker/btc-usd');
    // console.log("fetch promise is done!!!!!!");
-    const data  = await res.json();
+    //const data  = await res.json();
     //console.log("Json is done!!!!!!");
     //console.log("*********************");
-    console.log(data);
+    console.log(res ,"\n", typeof(res));
     }
     catch(e){
     console.log("Something went wrong!!!!!!!!!!!!!",e);
